cd8c53274db49c8751cae7447d7398d3dbda1603
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-11-01 18:02:35 +0300

    devops switch

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""get the top ten posts of a subreddit"""
+import os
+import requests
+
+
+def top_ten(subreddit):
+    """subscriber numbers"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=9".format(subreddit)
+    headers = {'User-Agent': 'Alxdevs2'}
+    response = requests.get(url, headers=headers)
+    if response.url != url:
+        print("None")
+        return
+    response = response.json()
+    if response.get('kind') != "Listing":
+        print("None")
+        return
+    data = response.get('data')
+    children = data.get('children')
+    count = 0
+    for child in children:
+        if count == 10:
+            return
+        print(child.get('data').get('title'))
+        count += 1
