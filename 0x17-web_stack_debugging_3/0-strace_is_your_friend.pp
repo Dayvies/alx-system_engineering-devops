b27a1f887ad013c0ace6f5815e17b1fae920f26e
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-11-02 21:43:19 +0300

    puppet

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+exec { 'fix-wordpress':
+  command => "sed -i '\\|^<Directory /var/www/>|a DirectoryIndex index.php' /etc/apache2/apache2.conf;
+   sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin',
+}
