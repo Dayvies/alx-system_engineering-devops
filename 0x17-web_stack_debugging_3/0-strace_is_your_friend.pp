ac1c3dc6822d24b95895a863b8c5a72e503b37ef
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-11-02 21:47:00 +0300

    puppet debug add comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+#puppet manifests to fix a wordpress blog
 exec { 'fix-wordpress':
   command => "sed -i '\\|^<Directory /var/www/>|a DirectoryIndex index.php' /etc/apache2/apache2.conf;
    sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
